fe3fb2c8fabd2ffb9ed87d253bbd34fa436cee3d
Krishna Vanam <venkatakrishnachaitanyavanam@Admins-MacBook-Pro.local>
2020-05-03 03:16:17 -0400

    adding login component

---
+++ src/pages/Login/Login.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { connect } from "react-redux";
+
+const LoginPage = (props) => {
+  return (
+    <>
+      <div className="container" style={{ paddingTop: "6rem" }}>
+        <h1>test</h1>
+      </div>
+    </>
+  );
+};
+
+export default LoginPage;
